eb8435393017a598bef32459e18ac6b7ec3b8aeb
iWas-Coder <wasymatieh01@gmail.com>
2024-03-05 01:55:32 +0100

    fix: protected all macro's parameters with parenthesis

--- include/carbon_should.h
+++ include/carbon_should.h
@@ -2,31 +2,31 @@
 
 #define carbon_should_be(expected, actual)                              \
   {                                                                     \
-    if (expected != actual) {                                            \
+    if ((expected) != (actual)) {                                        \
       CARBON_ERROR("%s:%d :: FAILED -> got '%d', expected '%d'\n",      \
                    __FILE__,                                            \
                    __LINE__,                                            \
-                   actual,                                              \
-                   expected);                                           \
+                   (actual),                                            \
+                   (expected));                                         \
       return false;                                                     \
     }                                                                   \
   }
 
 #define carbon_should_not_be(expected, actual)                          \
   {                                                                     \
-    if (expected == actual) {                                            \
+    if ((expected) == (actual)) {                                        \
       CARBON_ERROR("%s:%d :: FAILED -> got '%d == %d', expected not to\n", \
                    __FILE__,                                            \
                    __LINE__,                                            \
-                   actual,                                              \
-                   expected);                                           \
+                   (actual),                                            \
+                   (expected));                                         \
       return false;                                                     \
     }                                                                   \
   }
 
 #define carbon_should_be_true(actual)                                   \
   {                                                                     \
-    if (actual != true) {                                                \
+    if ((actual) != true) {                                              \
       CARBON_ERROR("%s:%d :: FAILED -> got 'false', expected 'true'\n", \
                    __FILE__,                                            \
                    __LINE__);                                           \
@@ -36,7 +36,7 @@
 
 #define carbon_should_be_false(actual)                                  \
   {                                                                     \
-    if (actual != false) {                                               \
+    if ((actual) != false) {                                             \
       CARBON_ERROR("%s:%d :: FAILED -> got 'true', expected 'false'\n", \
                    __FILE__,                                            \
                    __LINE__);                                           \
