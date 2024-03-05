5f2738e6acd3b186e3595c8b8c585eaeb392469b
iWas-Coder <wasymatieh01@gmail.com>
2024-03-05 23:10:52 +0100

    Created basic unit tests for `test_manager` logic

---
+++ test/src/carbon_test_manager_test.c
@@ -0,0 +1,35 @@
+#include <carbon.h>
+#include <carbon_test_manager_test.h>
+
+bool_t test_dummy(void) {
+  carbon_should_be_true(1 == 1);
+  return true;
+}
+
+bool_t carbon_test_manager_test_suite_creation(void) {
+  Suite s = carbon_test_manager_spawn();
+  carbon_should_be_p(0, s.tests);
+  carbon_should_be(0, s.n);
+  return true;
+}
+
+bool_t carbon_test_manager_test_registration(void) {
+  Suite s = carbon_test_manager_spawn();
+  carbon_should_be_p(0, s.tests);
+  carbon_should_be(0, s.n);
+
+  carbon_test_manager_register_s(&s, test_dummy, "test_dummy");
+  carbon_should_not_be_p(0, s.tests);
+  carbon_should_be(1, s.n);
+  carbon_should_be_s("test_dummy", s.tests[0].name);
+
+  carbon_test_manager_cleanup_s(&s);
+  carbon_should_be_p(0, s.tests);
+  carbon_should_be(0, s.n);
+  return true;
+}
+
+void carbon_test_manager_test_register(void) {
+  CARBON_REGISTER_TEST(carbon_test_manager_test_suite_creation);
+  CARBON_REGISTER_TEST(carbon_test_manager_test_registration);
+}
--- test/src/test.c
+++ test/src/test.c
@@ -1,8 +1,10 @@
 #include <carbon.h>
 #include <carbon_should_test.h>
+#include <carbon_test_manager_test.h>
 
 int main(void) {
   carbon_should_test_register();
+  carbon_test_manager_test_register();
 
   return carbon_test_manager_run();
 }
