5bc74bc81f6e4f47a816f2a00fc2bc8eabc1ff75
iWas-Coder <wasymatieh01@gmail.com>
2024-07-20 02:00:23 +0200

    Passthrough both source file for context and test file to extend

--- ai/testgen.cc
+++ ai/testgen.cc
@@ -1,32 +1,53 @@
 #include <sstream>
 #include <fstream>
 #include <iostream>
+#include <filesystem>
 #include "vendor/gemma.cpp/util/app.h"
 #include "vendor/gemma.cpp/util/args.h"
 #include "vendor/gemma.cpp/gemma/gemma.h"
 #include "vendor/gemma.cpp/evals/benchmark_helper.h"
 
 static constexpr auto system_prompt {
-  "Strictly follow the following instructions and rules:"
-  "- Just write the function that has been requested, no main, no examples, no nonsense."
-  "- When finished writing the function, do not repeat it, just write it once."
-  "- Do not explain anything, just write the code requested."
-  "- Do not use any markdown formatting at all, just plain text."
-  "- Only write the plain text code with no additional formatting."
-  "- Always use 2 space indenting, no tabs."
-  "- Do not write multiple blocks of code, just one."
-  "- Do not use any third-party dependency, just built-in features."
-  "Taking into consideration all of the instructions above, perform the following order as strictly as possible:"
+  "Strictly follow the following instructions and rules:\n"
+  "- Just write the function that has been requested, no main, no examples, no nonsense.\n"
+  "- When finished writing the function, do not repeat it, just write it once.\n"
+  "- Do not explain anything, just write the code requested.\n"
+  "- Do not use any markdown formatting at all, just plain text.\n"
+  "- Only write the plain text code with no additional formatting.\n"
+  "- Always use 2 space indenting, no tabs.\n"
+  "- Do not write multiple blocks of code, just one.\n"
+  "- Do not use any third-party dependency, just built-in features.\n"
+  "Take all of these instructions into consideration while performing as accurate as possible the following requests.\n"
+};
+static constexpr auto test_code_prompt {
+  "Here is a C/C++ unit test translation unit:"
+};
+static constexpr auto src_code_prompt {
+  "And this is the C/C++ translation unit that it tests:"
+};
+static constexpr auto request_prompt {
+  "Write an extended version of the unit test translation unit that includes additional unit tests that will increase the test coverage of the code under test.\n"
 };
 
-static constexpr auto user_prompt {
-  "C++. We have a function that computes GCD of pair of numbers. Write a unit test for it."
-};
+static inline std::string load_file_contents(const std::string &filepath) {
+  std::ifstream ifs { filepath };
+  if (not ifs) throw std::runtime_error { "unable to open file for reading (`" + filepath + "`)" };
+  std::stringstream ss;
+  ss << "```\n" << ifs.rdbuf() << "\n```\n";
+  return ss.str();
+}
 
-static inline std::vector<int> tokenize(const std::string &prompt, const gcpp::GemmaTokenizer &tokenizer) {
-  std::string ctx { std::string(system_prompt) + "\n" + prompt + "\n" };
+static inline std::vector<int> tokenize(const gcpp::GemmaTokenizer &tokenizer, const std::string &test_code, const std::string &src_code) {
+  std::string prompt {
+    std::string(system_prompt) + "\n" +
+    test_code_prompt + "\n" +
+    test_code + "\n" +
+    src_code_prompt + "\n" +
+    src_code + "\n" +
+    request_prompt + "\n"
+  };
   std::vector<int> tokens;
-  HWY_ASSERT(tokenizer.Encode(ctx, &tokens));
+  HWY_ASSERT(tokenizer.Encode(prompt, &tokens));
   tokens.insert(tokens.begin(), gcpp::BOS_ID);
   return tokens;
 }
@@ -67,34 +88,61 @@ int main(int argc, char **argv) {
   inference.deterministic = false;
   inference.multiturn = false;
 
-  gcpp::AppArgs app(argc, argv);
+  gcpp::AppArgs app { argc, argv };
   app.num_threads = 1;
   if (argc >= 3 and argv[1] == std::string("-t")) app.num_threads = std::atoi(argv[2]);
   else std::cout << "WARNING: using 1 thread for inference by default.\n" << std::endl;
+  hwy::ThreadPool thread_pool(app.num_threads);
+  if (app.num_threads > 10) gcpp::PinWorkersToCores(thread_pool);
 
-  if (const char* err { loader.Validate() }) HWY_ABORT("%s", err);
-  if (const char* err { inference.Validate() }) HWY_ABORT("%s", err);
+  std::string src_file, test_file;
+  switch (argc) {
+  case 5:
+    if (argv[1] != std::string("--src")) HWY_ABORT("usage: %s [-t <N>] --src <FILE> --test <FILE>", argv[0]);
+    if (not std::filesystem::exists(argv[2])) HWY_ABORT("usage: %s [-t <N>] --src <FILE> --test <FILE>", argv[0]);
+    if (argv[3] != std::string("--test")) HWY_ABORT("usage: %s [-t <N>] --src <FILE> --test <FILE>", argv[0]);
+    if (not std::filesystem::exists(argv[4])) HWY_ABORT("usage: %s [-t <N>] --src <FILE> --test <FILE>", argv[0]);
+    src_file = argv[2];
+    test_file = argv[4];
+    break;
+  case 7:
+    if (argv[3] != std::string("--src")) HWY_ABORT("usage: %s [-t <N>] --src <FILE> --test <FILE>", argv[0]);
+    if (not std::filesystem::exists(argv[4])) HWY_ABORT("usage: %s [-t <N>] --src <FILE> --test <FILE>", argv[0]);
+    if (argv[5] != std::string("--test")) HWY_ABORT("usage: %s [-t <N>] --src <FILE> --test <FILE>", argv[0]);
+    if (not std::filesystem::exists(argv[6])) HWY_ABORT("usage: %s [-t <N>] --src <FILE> --test <FILE>", argv[0]);
+    src_file = argv[4];
+    test_file = argv[6];
+    break;
+  default:
+    HWY_ABORT("usage: %s [-t <N>] --src <FILE> --test <FILE>", argv[0]);
+  }
 
-  hwy::ThreadPool pool(app.num_threads);
-  if (app.num_threads > 10) gcpp::PinWorkersToCores(pool);
-  gcpp::Gemma model { gcpp::CreateGemma(loader, pool) };
+  if (const char* err { loader.Validate() }) HWY_ABORT("%s:%u :: %s", __FILE__, __LINE__, err);
+  if (const char* err { inference.Validate() }) HWY_ABORT("%s:%u :: %s", __FILE__, __LINE__, err);
+
+  std::string src_file_contents { load_file_contents(src_file) };
+  std::string test_file_contents { load_file_contents(test_file) };
+
+  gcpp::Gemma model { gcpp::CreateGemma(loader, thread_pool) };
   gcpp::KVCache kv_cache { gcpp::KVCache::Create(model.Info().model) };
   gcpp::TimingInfo timings;
   std::random_device rand_dev;
   std::mt19937 rand_gen { rand_dev() };
 
-  std::vector<int> prompt { tokenize(user_prompt, model.Tokenizer()) };
+  std::vector<int> prompt { tokenize(model.Tokenizer(), test_file_contents, src_file_contents) };
   size_t prompt_size { prompt.size() };
   size_t pos {0};
   std::stringstream buf;
   auto stream_token = [prompt_size, &pos, &buf, &model](int token, float) {
     ++pos;
     if (pos > prompt_size and token != gcpp::EOS_ID) {
+      if (pos == prompt_size + 1) std::cout << std::endl << std::flush;
       std::string tok;
       HWY_ASSERT(model.Tokenizer().Decode(std::vector<int>{token}, &tok));
       buf << tok;
       std::cout << tok << std::flush;
     }
+    else std::cout << "." << std::flush;
     return true;
   };
 
