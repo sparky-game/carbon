68aec31b5925f337bfc49bf86b424581b9618ca3
iWas-Coder <wasymatieh01@gmail.com>
2024-10-19 19:17:07 +0200

    Added ASCII art box + simplified license info
    Full license is in separate file, now each file features the SPDX
    License Identifier (`BSD-3-Clause`).

--- src/carbon_main.h
+++ src/carbon_main.h
@@ -1,23 +1,5 @@
-/*
- * BSD Carbon --- A simple C/C++ unit testing framework
- * Copyright (C) 2024 Wasym A. Alonso
- *
- * This file is part of Carbon.
- *
- * Carbon is free software: you can redistribute it and/or modify
- * it under the terms of the BSD 3-Clause "New" or "Revised" License
- * as published by The Regents of the University of California.
- *
- * Carbon is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * BSD 3-Clause "New" or "Revised" License for more details.
- *
- * You should have received a copy of the BSD 3-Clause "New" or
- * "Revised" License along with Carbon.
- * If not, see <https://opensource.org/license/BSD-3-Clause>.
- */
-
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (C) Wasym A. Alonso. All Rights Reserved.
 
 #pragma once
 
