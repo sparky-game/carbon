f20cba8c45da91ab2627fd2af797e45e8c25e0ee
iWas-Coder <wasymatieh01@gmail.com>
2024-10-20 02:03:10 +0200

    fix: print test `should` errors with red color

--- src/carbon_should.h
+++ src/carbon_should.h
@@ -8,7 +8,7 @@
 #define carbon_should_be(expected, actual)                              \
   {                                                                     \
     if ((int) (expected) != (int) (actual)) {                            \
-      CARBON_ERROR("%s:%d :: FAILED -> got '%d', expected '%d'\n",      \
+      CARBON_ERROR(CARBON_COLOR_RED "%s:%d :: FAILED -> got '%d', expected '%d'" CARBON_COLOR_RESET " \n", \
                    __FILE__,                                            \
                    __LINE__,                                            \
                    (int) (actual),                                      \
@@ -20,7 +20,7 @@
 #define carbon_should_not_be(expected, actual)                          \
   {                                                                     \
     if ((int) (expected) == (int) (actual)) {                            \
-      CARBON_ERROR("%s:%d :: FAILED -> got '%d == %d', expected not to\n", \
+      CARBON_ERROR(CARBON_COLOR_RED "%s:%d :: FAILED -> got '%d == %d', expected not to" CARBON_COLOR_RESET "\n", \
                    __FILE__,                                            \
                    __LINE__,                                            \
                    (int) (actual),                                      \
@@ -32,7 +32,7 @@
 #define carbon_should_be_lt(expected, actual)                           \
   {                                                                     \
     if ((int) (expected) <= (int) (actual)) {                            \
-      CARBON_ERROR("%s:%d :: FAILED -> got '%d >= %d', expected '%d < %d'\n", \
+      CARBON_ERROR(CARBON_COLOR_RED "%s:%d :: FAILED -> got '%d >= %d', expected '%d < %d'" CARBON_COLOR_RESET "\n", \
                    __FILE__,                                            \
                    __LINE__,                                            \
                    (int) (actual),                                      \
@@ -46,7 +46,7 @@
 #define carbon_should_be_le(expected, actual)                           \
   {                                                                     \
     if ((int) (expected) < (int) (actual)) {                            \
-      CARBON_ERROR("%s:%d :: FAILED -> got '%d > %d', expected '%d <= %d'\n", \
+      CARBON_ERROR(CARBON_COLOR_RED "%s:%d :: FAILED -> got '%d > %d', expected '%d <= %d'" CARBON_COLOR_RESET "\n", \
                    __FILE__,                                            \
                    __LINE__,                                            \
                    (int) (actual),                                      \
@@ -60,7 +60,7 @@
 #define carbon_should_be_gt(expected, actual)                           \
   {                                                                     \
     if ((int) (expected) >= (int) (actual)) {                            \
-      CARBON_ERROR("%s:%d :: FAILED -> got '%d <= %d', expected '%d > %d'\n", \
+      CARBON_ERROR(CARBON_COLOR_RED "%s:%d :: FAILED -> got '%d <= %d', expected '%d > %d'" CARBON_COLOR_RESET "\n", \
                    __FILE__,                                            \
                    __LINE__,                                            \
                    (int) (actual),                                      \
@@ -74,7 +74,7 @@
 #define carbon_should_be_ge(expected, actual)                           \
   {                                                                     \
     if ((int) (expected) > (int) (actual)) {                            \
-      CARBON_ERROR("%s:%d :: FAILED -> got '%d < %d', expected '%d >= %d'\n", \
+      CARBON_ERROR(CARBON_COLOR_RED "%s:%d :: FAILED -> got '%d < %d', expected '%d >= %d'" CARBON_COLOR_RESET "\n", \
                    __FILE__,                                            \
                    __LINE__,                                            \
                    (int) (actual),                                      \
@@ -88,7 +88,7 @@
 #define carbon_should_be_p(expected, actual)                            \
   {                                                                     \
     if ((void *) (expected) != (void *) (actual)) {                      \
-      CARBON_ERROR("%s:%d :: FAILED -> got '%p', expected '%p'\n",      \
+      CARBON_ERROR(CARBON_COLOR_RED "%s:%d :: FAILED -> got '%p', expected '%p'" CARBON_COLOR_RESET "\n", \
                    __FILE__,                                            \
                    __LINE__,                                            \
                    (void *) (actual),                                   \
@@ -100,7 +100,7 @@
 #define carbon_should_not_be_p(expected, actual)                        \
   {                                                                     \
     if ((void *) (expected) == (void *) (actual)) {                      \
-      CARBON_ERROR("%s:%d :: FAILED -> got '%p == %p', expected not to\n", \
+      CARBON_ERROR(CARBON_COLOR_RED "%s:%d :: FAILED -> got '%p == %p', expected not to" CARBON_COLOR_RESET "\n", \
                    __FILE__,                                            \
                    __LINE__,                                            \
                    (void *) (actual),                                   \
@@ -112,7 +112,7 @@
 #define carbon_should_be_s(expected, actual)                            \
   {                                                                     \
     if (strcmp((expected), (actual))) {                                 \
-      CARBON_ERROR("%s:%d :: FAILED -> got '%s', expected '%s'\n",      \
+      CARBON_ERROR(CARBON_COLOR_RED "%s:%d :: FAILED -> got '%s', expected '%s'" CARBON_COLOR_RESET "\n", \
                    __FILE__,                                            \
                    __LINE__,                                            \
                    (actual),                                            \
@@ -124,7 +124,7 @@
 #define carbon_should_not_be_s(expected, actual)                        \
   {                                                                     \
     if (!strcmp((expected), (actual))) {                                \
-      CARBON_ERROR("%s:%d :: FAILED -> got '%s == %s', expected not to\n", \
+      CARBON_ERROR(CARBON_COLOR_RED "%s:%d :: FAILED -> got '%s == %s', expected not to" CARBON_COLOR_RESET "\n", \
                    __FILE__,                                            \
                    __LINE__,                                            \
                    (actual),                                            \
@@ -136,7 +136,7 @@
 #define carbon_should_be_true(actual)                                   \
   {                                                                     \
     if (!(actual)) {                                                    \
-      CARBON_ERROR("%s:%d :: FAILED -> got 'false', expected 'true'\n", \
+      CARBON_ERROR(CARBON_COLOR_RED "%s:%d :: FAILED -> got 'false', expected 'true'" CARBON_COLOR_RESET "\n", \
                    __FILE__,                                            \
                    __LINE__);                                           \
       return CARBON_KO;                                                 \
@@ -146,7 +146,7 @@
 #define carbon_should_be_false(actual)                                  \
   {                                                                     \
     if ((actual)) {                                                     \
-      CARBON_ERROR("%s:%d :: FAILED -> got 'true', expected 'false'\n", \
+      CARBON_ERROR(CARBON_COLOR_RED "%s:%d :: FAILED -> got 'true', expected 'false'" CARBON_COLOR_RESET "\n", \
                    __FILE__,                                            \
                    __LINE__);                                           \
       return CARBON_KO;                                                 \
