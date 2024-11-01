d520799593f2f80d10cacf4422e55e2b666f9b9e
iWas-Coder <wasymatieh01@gmail.com>
2024-11-01 03:36:31 +0100

    fix: improved looks of demo GIF

--- assets/run-tests-demo.exs
+++ assets/run-tests-demo.exs
@@ -4,9 +4,14 @@
 Output assets/run-tests-demo.gif
 
 Set Width 1000
-Set Height 770
+Set Height 850
+Set Margin 20
+Set MarginFill "#f9a85e"
+Set BorderRadius 10
+Set WindowBar Colorful
 Set FontSize 22
 Set TypingSpeed 75ms
+Set Framerate 60
 
 Type "./test/carbon"
 Sleep 500ms
