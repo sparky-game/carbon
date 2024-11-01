715fb84e0de826f8d9479b1e067913b780ec0633
iWas-Coder <wasymatieh01@gmail.com>
2024-11-01 01:27:25 +0100

    Refactor build script with new funcs + some cleanup

--- src/make.c
+++ src/make.c
@@ -5,8 +5,6 @@
 #define CARBON_IMPLEMENTATION
 #include "../carbon.h"
 
-#include <glob.h>
-
 #define WORKDIR                                         \
   "carbon"                                              \
   "-" CARBON_EXPAND_AND_QUOTE(CARBON_VERSION_MAJOR)     \
@@ -15,83 +13,72 @@
   "-" CARBON_CPU_ARCH
 
 static void call_cmd(const char *cmd) {
-  CARBON_INFO("+ %s", cmd);
   if (!system(cmd)) return;
   CARBON_ERROR("Unable to run `%s`", cmd);
   exit(1);
 }
 
 static inline void rm_dash_r(const char *path) {
+  CARBON_INFO("  RM      %s", path);
   call_cmd(carbon_string_fmt("rm -rf %s", path));
 }
 
 static inline void cp_dash_r(const char *origin, const char *dest) {
+  CARBON_INFO("  CP      %s", dest);
   call_cmd(carbon_string_fmt("cp -r %s %s", origin, dest));
 }
 
 static void build_src_files(void) {
-  glob_t glob_result;
-  glob("src/carbon_*.c", GLOB_TILDE, 0, &glob_result);
-  for (usz i = 0; i < glob_result.gl_pathc; ++i) {
-    CARBON_INFO("  CC      %s", glob_result.gl_pathv[i]);
-    carbon_string_strip_substr(glob_result.gl_pathv[i], "src/");
-    carbon_string_strip_substr(glob_result.gl_pathv[i], ".c");
-    const char *cmd = carbon_string_fmt(CARBON_COMPILER " -I . -std=gnu99 -Wall -Wextra -pipe -O3 -c src/%s.c -o %s/%s.o",
-                                        glob_result.gl_pathv[i], WORKDIR, glob_result.gl_pathv[i]);
-    if (!system(cmd)) continue;
-    CARBON_ERROR("Errors when compiling the code");
-    rm_dash_r("build");
-    exit(1);
+  usz files_count = 0;
+  char **files = carbon_fs_pattern_match("src/carbon_*.c", &files_count);
+  for (usz i = 0; i < files_count; ++i) {
+    CARBON_INFO("  CC      %s", files[i]);
+    carbon_string_strip_substr(files[i], "src/");
+    carbon_string_strip_substr(files[i], ".c");
+    call_cmd(carbon_string_fmt(CARBON_COMPILER " -I . -std=gnu99 -Wall -Wextra -pipe -O3 -c src/%s.c -o %s/%s.o", files[i], WORKDIR, files[i]));
   }
 }
 
-static inline void strip_objs(const char *path) {
-  call_cmd(carbon_string_fmt("strip %s", path));
-}
-
-static void create_static_lib(const char *name) {
-  strip_objs(WORKDIR "/*.o");
-  CARBON_INFO("  AR      %s", name);
-  if (!system(carbon_string_fmt("ar -rcs %s/%s %s/*.o", WORKDIR, name, WORKDIR))) {
-    rm_dash_r(WORKDIR "/*.o");
-    return;
-  }
-  CARBON_ERROR("Unable to create static lib `%s`", name);
-  exit(1);
+static void create_static_lib(void) {
+  CARBON_INFO("  STRIP   " WORKDIR "/*.o");
+  call_cmd(carbon_string_fmt("strip " WORKDIR "/*.o"));
+  CARBON_INFO("  AR      libcarbon.a");
+  call_cmd("ar -rcs " WORKDIR "/libcarbon.a " WORKDIR "/*.o");
+  rm_dash_r(WORKDIR "/*.o");
 }
 
 static inline void compress_dir(const char *path) {
+  CARBON_INFO("  GZIP   %s.tgz", path);
   call_cmd(carbon_string_fmt("tar -zcf %s.tgz %s", path, path));
 }
 
 static void run_tests(void) {
-  const char *cmd = CARBON_COMPILER " -I . -std=gnu99 -Wall -Wextra -fsanitize=address,undefined test/*.c -o test/carbon";
+  const char *cc_cmd = CARBON_COMPILER " -I . -std=gnu99 -Wall -Wextra -fsanitize=address,undefined test/*.c -o test/carbon";
+  const char *test_cmd = "./test/carbon -n";
   CARBON_INFO("  CCLD    test/carbon");
-  if (!system(cmd)) {
-    call_cmd("./test/carbon -n");
-    return;
-  }
-  CARBON_ERROR("Errors when compiling the code");
-  exit(1);
+  call_cmd(cc_cmd);
+  CARBON_INFO("+ %s", test_cmd);
+  call_cmd(test_cmd);
+  return;
 }
 
 static void build_examples(void) {
-  glob_t glob_result;
-  glob("examples/*.c", GLOB_TILDE, 0, &glob_result);
-  for (usz i = 0; i < glob_result.gl_pathc; ++i) {
-    carbon_string_strip_substr(glob_result.gl_pathv[i], ".c");
-    CARBON_INFO("  CCLD    %s", glob_result.gl_pathv[i]);
-    const char *cmd = carbon_string_fmt(CARBON_COMPILER " -std=gnu99 -Wall -Wextra -pipe -O3 %s.c -static -o %s",
-                                        glob_result.gl_pathv[i], glob_result.gl_pathv[i]);
-    if (!system(cmd)) continue;
-    CARBON_ERROR("Errors when compiling the code");
-    exit(1);
+  usz files_count = 0;
+  char **files = carbon_fs_pattern_match("examples/*.c", &files_count);
+  for (usz i = 0; i < files_count; ++i) {
+    carbon_string_strip_substr(files[i], ".c");
+    CARBON_INFO("  CCLD    %s", files[i]);
+    call_cmd(carbon_string_fmt(CARBON_COMPILER " -std=gnu99 -Wall -Wextra -pipe -O3 %s.c -static -o %s", files[i], files[i]));
   }
 }
 
 static inline void clean(void) {
-  rm_dash_r("test/carbon " WORKDIR " " WORKDIR ".tgz");
-  call_cmd(carbon_string_fmt("find examples -type f -executable -delete"));
+  rm_dash_r("test/carbon");
+  rm_dash_r(WORKDIR);
+  rm_dash_r(WORKDIR ".tgz");
+  const char *delete_cmd = "find examples -type f -executable -delete";
+  CARBON_INFO("+ %s", delete_cmd);
+  call_cmd(carbon_string_fmt(delete_cmd));
 }
 
 static inline void mrproper(void) {
@@ -112,17 +99,17 @@ static void handle_args(int argc, char **argv) {
 }
 
 static void build(void) {
-  CARBON_INFO("+ mkdir " WORKDIR);
+  CARBON_INFO("  MKDIR   " WORKDIR);
   if (!carbon_fs_create_directory(WORKDIR)) {
     clean();
     exit(1);
   }
   build_src_files();
-  create_static_lib("libcarbon.a");
+  create_static_lib();
 }
 
 static void package(void) {
-  CARBON_INFO("+ mkdir " WORKDIR "/src");
+  CARBON_INFO("  MKDIR   " WORKDIR "/src");
   if (!carbon_fs_create_directory(WORKDIR "/src")) {
     clean();
     exit(1);
@@ -146,6 +133,6 @@ int main(int argc, char **argv) {
   CARBON_INFO_COLOR(CARBON_COLOR_YELLOW, "[*] Building and packaging...");
   build();
   package();
-  CARBON_INFO_COLOR(CARBON_COLOR_YELLOW, "[*] Output: " WORKDIR ".tgz");
+  CARBON_INFO_COLOR(CARBON_COLOR_YELLOW, "[*] Output: " WORKDIR ".tgz is ready");
   return 0;
 }
