7b0aacd481f44f5c71e091aa698696878d254cdf
iWas-Coder <wasymatieh01@gmail.com>
2024-11-01 02:32:42 +0100

    Added GIF as demo of running tests
    Thanks to Charm for such an amazing tool: https://github.com/charmbracelet/vhs

---
+++ assets/run-tests-demo.exs
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (C) Wasym A. Alonso. All Rights Reserved.
+
+Output assets/run-tests-demo.gif
+
+Set Width 1000
+Set Height 770
+Set FontSize 22
+Set TypingSpeed 75ms
+
+Type "./test/carbon"
+Sleep 500ms
+Enter
+Sleep 5s
