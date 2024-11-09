d92bb274fc82a17aeed0437777585b9f01dcda79
iWas-Coder <wasymatieh01@gmail.com>
2024-11-09 21:32:37 +0100

    fix: forgot to create the network's matrices

--- src/carbon_nn.c
+++ src/carbon_nn.c
@@ -18,6 +18,12 @@ CBN_NeuralNet carbon_nn_create(usz *arch, usz arch_count) {
     CARBON_ERROR("failed to allocate memory");
     memset(&nn, 0, sizeof(CBN_NeuralNet));
   }
+  nn.as[0] = carbon_math_row_create(arch[0]);
+  for (usz i = 1; i < arch_count; ++i) {
+    nn.ws[i - 1] = carbon_math_mat_create(nn.as[i - 1].cols, arch[i]);
+    nn.bs[i - 1] = carbon_math_row_create(arch[i]);
+    nn.as[i]     = carbon_math_row_create(arch[i]);
+  }
   return nn;
 }
 
