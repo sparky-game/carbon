607b68de63488ebeca328c8896e36b07f3ae2cbd
iWas-Coder <wasymatieh01@gmail.com>
2024-11-23 23:12:07 +0100

    Definitive license change to `AGPL-3.0-only`
    Also, possibility of obtaining a Carbon RLE (GPL Exception).

--- assets/run-tests-demo.exs
+++ assets/run-tests-demo.exs
@@ -1,4 +1,4 @@
-# SPDX-License-Identifier: BSD-3-Clause OR GPL-3.0-only
+# SPDX-License-Identifier: AGPL-3.0-only
 # Copyright (C) Wasym A. Alonso. All Rights Reserved.
 
 Output assets/run-tests-demo.gif
