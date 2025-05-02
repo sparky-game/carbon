3ed14644fcef4f5c691e50d6ea527cca92480bb7
iWas-Coder <wasymatieh01@gmail.com>
2025-05-02 23:10:58 +0200

    Added `RGFW` dependency to the package (tarball)

--- src/make.c
+++ src/make.c
@@ -29,17 +29,20 @@ static const char * const help_msg = "usage: %s [SUBCMD]\n"
   "Report bugs to: <https://github.com/sparky-game/carbon/issues>\n"
   "%s homepage: <https://github.com/sparky-game/carbon>\n";
 
+// TODO: migrate this func to the lib itself
 static inline void call_cmd(const char *cmd) {
   if (!system(cmd)) return;
   carbon_log_error("Unable to run `%s`", cmd);
   exit(1);
 }
 
+// TODO: migrate this func to the lib itself
 static inline void rm_dash_r(const char *path) {
   carbon_println("  RM      %s", path);
   call_cmd(carbon_string_fmt("rm -rf %s", path));
 }
 
+// TODO: migrate this func to the lib itself
 static inline void cp_dash_r(const char *origin, const char *dest) {
   carbon_println("  CP      %s -> %s", origin, dest);
   call_cmd(carbon_string_fmt("cp -r %s %s", origin, dest));
@@ -201,6 +204,11 @@ static void package(void) {
   cp_dash_r(dir "/*", WORKDIR "/" dir);
 #undef dir
 #define dir "vendor/stb_image_write"
+  carbon_println("  MKDIR   " WORKDIR "/" dir);
+  if (!carbon_fs_create_directories(WORKDIR "/" dir)) exit_gracefully();
+  cp_dash_r(dir "/*", WORKDIR "/" dir);
+#undef dir
+#define dir "vendor/RGFW"
   carbon_println("  MKDIR   " WORKDIR "/" dir);
   if (!carbon_fs_create_directories(WORKDIR "/" dir)) exit_gracefully();
   cp_dash_r(dir "/*", WORKDIR "/" dir);
