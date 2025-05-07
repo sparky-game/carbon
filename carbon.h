f8a85b723963fcf15023a851eda5e25efcfe0f16
iWas-Coder <wasymatieh01@gmail.com>
2025-05-07 23:40:31 +0200

    Removed unnecessary feature macros

--- carbon.h
+++ carbon.h
@@ -48,10 +48,6 @@
 **                               as a test suite entrypoint. It's only relevant to
 **                               define it in the translation unit where we're defining
 **                               the `main` function.
-**    - CARBON_USE_COROUTINES -> Enable extra functionality to work with coroutines and
-**                               do async work in a lightweight way.
-**    - CARBON_USE_WINDOWING --> Enable extra functionality to work with native windows
-**                               and make GUI applications.
 */
 
 /*
@@ -983,9 +979,7 @@ CARBON_API void carbon_junit_output(const CBN_List junit_tcs, const char *out_fi
 #ifdef CARBON_IMPLEMENTATION
 #include "src/carbon_assert.c"
 #include "src/carbon_memory.c"
-#ifdef CARBON_USE_COROUTINES
 #include "src/carbon_coroutine.c"
-#endif  // CARBON_USE_COROUTINES
 #include "src/carbon_math.c"
 #include "src/carbon_math_mfns.cc"
 #include "src/carbon_math_ops.cc"
@@ -1002,9 +996,7 @@ CARBON_API void carbon_junit_output(const CBN_List junit_tcs, const char *out_fi
 #include "src/carbon_fs.c"
 #include "src/carbon_nn.c"
 #include "src/carbon_drawcanvas.c"
-#ifdef CARBON_USE_WINDOWING
 #include "src/carbon_win.c"
-#endif  // CARBON_USE_WINDOWING
 #include "src/carbon_test_manager.c"
 #include "src/carbon_junit.c"
 #endif  // CARBON_IMPLEMENTATION
