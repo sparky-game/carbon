089bdef46e9f3e92a2f95de9dcf753e010b7d3cd
iWas-Coder <wasymatieh01@gmail.com>
2025-06-07 20:36:01 +0200

    Added more C++ namespaced aliases

--- carbon.h
+++ carbon.h
@@ -2287,13 +2287,29 @@ CARBON_API usz carbon_skap_count_of(const CBN_SKAP_Handle *handle, const CBN_SKA
 */
 #ifdef __cplusplus
 namespace cbn {
-  using Vec2 = CBN_Vec2;
-  using Vec3 = CBN_Vec3;
-  using Rect = CBN_Rect;
+  const auto version = carbon_version;
+  using Vec2         = CBN_Vec2;
+  using Vec3         = CBN_Vec3;
+  using Rect         = CBN_Rect;
   template <typename T>
   using List       = CBN_List_t<T>;
   using StrBuilder = CBN_StrBuilder;
   using StrView    = CBN_StrView;
+  namespace mem {
+    const auto cpy = carbon_memory_copy;
+    const auto cmp = carbon_memory_cmp;
+  }
+  namespace str {
+    const auto len        = carbon_string_len;
+    const auto cmp        = carbon_string_cmp;
+    const auto ncmp       = carbon_string_cmp_n;
+    const auto dup        = carbon_string_dup;
+    const auto fmt        = carbon_string_fmt;
+    const auto StartsWith = carbon_string_starts_with_substr;
+    const auto EndsWith   = carbon_string_starts_with_substr;
+    const auto IsNumber   = carbon_string_is_number;
+    const auto LevDist    = carbon_string_lev_dist;
+  }
   namespace win {
     const auto Open         = carbon_win_open;
     const auto Close        = carbon_win_close;
