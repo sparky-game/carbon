e88603a305a79e2ab74b727bbebe7d7ad4e9fcf7
iWas-Coder <wasymatieh01@gmail.com>
2025-06-16 05:41:07 +0200

    Added new `cat` program example in C

---
+++ examples/cat.c
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+// Copyright (C) Wasym A. Alonso. All Rights Reserved.
+
+#include <carbon.h>
+
+int main(int argc, char **argv) {
+  const char *program_name = CARBON_SHIFT_ARGS(argc, argv);
+  if (!argc) {
+    carbon_log_error("usage: %$ [FILE...]", $(program_name));
+    return 1;
+  }
+  while (argc) {
+    const char *file = CARBON_SHIFT_ARGS(argc, argv);
+    if (!carbon_fs_is_regular_file(file)) {
+      carbon_log_error("FILE (`%$`) needs to be a regular file", $(file));
+      return 1;
+    }
+    CBN_StrBuilder sb = {0};
+    if (!carbon_fs_read_entire_file(&sb, file)) {
+      carbon_log_error("FILE (`%$`) could not be read", $(file));
+      return 1;
+    }
+    carbon_print("%.*s", sb.size, sb.items);
+    carbon_strbuilder_free(&sb);
+  }
+  return 0;
+}
