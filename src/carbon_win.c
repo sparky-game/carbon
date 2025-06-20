e7430cb11743c41fa2aa614732e65ec0667626b4
iWas-Coder <wasymatieh01@gmail.com>
2025-06-21 00:23:54 +0200

    Implemented the remaining Xrandr function wrappers

--- src/carbon_win.c
+++ src/carbon_win.c
@@ -1097,26 +1097,62 @@ XRRCrtcInfo *XRRGetCrtcInfo(Display *dpy, XRRScreenResources *resources, RRCrtc 
 }
 
 // Xrandr :: XRRFreeOutputInfo
-// ...
+CARBON_WIN__DLDECL(XRRFreeOutputInfo);
+void XRRFreeOutputInfo(XRROutputInfo *outputInfo) {
+  CARBON_WIN__DLSYM(carbon_win__dl_Xrandr, XRRFreeOutputInfo);
+  return XRRFreeOutputInfo_ptr(outputInfo);
+}
 
 // Xrandr :: XRRGetScreenResources
-// ...
+CARBON_WIN__DLDECL(XRRGetScreenResources);
+XRRScreenResources *XRRGetScreenResources(Display *dpy, Window window) {
+  CARBON_WIN__DLSYM(carbon_win__dl_Xrandr, XRRGetScreenResources);
+  return XRRGetScreenResources_ptr(dpy, window);
+}
 
 // Xrandr :: XRRFreeScreenResources
-// ...
+CARBON_WIN__DLDECL(XRRFreeScreenResources);
+void XRRFreeScreenResources(XRRScreenResources *resources) {
+  CARBON_WIN__DLSYM(carbon_win__dl_Xrandr, XRRFreeScreenResources);
+  return XRRFreeScreenResources_ptr(resources);
+}
 
 // Xrandr :: XRRSetCrtcConfig
-// ...
+CARBON_WIN__DLDECL(XRRSetCrtcConfig);
+Status XRRSetCrtcConfig(Display *dpy,
+                        XRRScreenResources *res,
+                        RRCrtc crtc,
+                        Time timestamp,
+                        int x,
+                        int y,
+                        RRMode mode,
+                        Rotation rotation,
+                        RROutput *outputs,
+                        int noutputs) {
+  CARBON_WIN__DLSYM(carbon_win__dl_Xrandr, XRRSetCrtcConfig);
+  return XRRSetCrtcConfig_ptr(dpy, res, crtc, timestamp, x, y, mode, rotation, outputs, noutputs);
+}
 
 // Xrandr :: XRRGetScreenResourcesCurrent
-// ...
+CARBON_WIN__DLDECL(XRRGetScreenResourcesCurrent);
+XRRScreenResources *XRRGetScreenResourcesCurrent(Display *dpy, Window window) {
+  CARBON_WIN__DLSYM(carbon_win__dl_Xrandr, XRRGetScreenResourcesCurrent);
+  return XRRGetScreenResourcesCurrent_ptr(dpy, window);
+}
 
 // Xrandr :: XRRFreeCrtcInfo
-// ...
+CARBON_WIN__DLDECL(XRRFreeCrtcInfo);
+void XRRFreeCrtcInfo(XRRCrtcInfo *crtcInfo) {
+  CARBON_WIN__DLSYM(carbon_win__dl_Xrandr, XRRFreeCrtcInfo);
+  return XRRFreeCrtcInfo_ptr(crtcInfo);
+}
 
 // Xrandr :: XRRGetOutputInfo
-// ...
-
+CARBON_WIN__DLDECL(XRRGetOutputInfo);
+XRROutputInfo *XRRGetOutputInfo(Display *dpy, XRRScreenResources *res, RROutput output) {
+  CARBON_WIN__DLSYM(carbon_win__dl_Xrandr, XRRGetOutputInfo);
+  return XRRGetOutputInfo_ptr(dpy, res, output);
+}
 #endif
 
 CARBON_INLINE void carbon_win__dl_open(void) {
