a4de7617d891a981ff43347c8f3069b03b40adfe
iWas-Coder <wasymatieh01@gmail.com>
2025-06-21 04:00:17 +0200

    Add default parameter to `coroutine_go` (C++)

--- src/carbon_coroutine.h
+++ src/carbon_coroutine.h
@@ -20,9 +20,29 @@ typedef enum {
   CBN_COROUTINE_SLEEP_MODE_WRITE
 } CBN_Coroutine_SleepMode;
 
+/**
+ * @brief Obtains the ID of the coroutine that governs the current scope.
+ * @return The ID.
+ */
 CARBON_API usz carbon_coroutine_id(void);
+
 CARBON_API usz carbon_coroutine_alive(void);
+
+/**
+ * @brief Creates a new coroutine.
+ *
+ * It doesn't automatically switch to it, it continues executing the current one.
+ * It'll get to it eventually via the `yield` operation.
+ *
+ * @param f The function containing the body of the coroutine.
+ * @param arg The arbitrary amount of arguments passed to `f`.
+ */
+#ifdef __cplusplus
+CARBON_API void carbon_coroutine_go(void (*f)(void *), void *arg = 0);
+#else
 CARBON_API void carbon_coroutine_go(void (*f)(void *), void *arg);
+#endif
+
 CARBON_API void carbon_coroutine_yield(void);
 CARBON_API void carbon_coroutine_sleep_read(i32 fd);
 CARBON_API void carbon_coroutine_sleep_write(i32 fd);
