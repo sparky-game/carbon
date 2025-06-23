7c6cdc08c17e3e17743df3eb1d20b0d29ebc5835
iWas-Coder <wasymatieh01@gmail.com>
2025-06-23 16:51:19 +0200

    Added basic hello example
    Signed-off-by: iWas-Coder <wasymatieh01@gmail.com>

---
+++ examples/hello.c
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+// Copyright (C) Wasym A. Alonso. All Rights Reserved.
+
+#include <carbon.h>
+
+int main(void) {
+  carbon_println("Hello, World!");
+  return 0;
+}
