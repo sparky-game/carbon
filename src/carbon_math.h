802b5c621c1a9af6ee9518858c09bd92b635ebc9
iWas-Coder <wasymatieh01@gmail.com>
2025-06-27 06:02:16 +0200

    Added docstrings to some math macros

--- src/carbon_math.h
+++ src/carbon_math.h
@@ -9,39 +9,81 @@
 
 #pragma once
 
-#define CARBON_E 2.71828182845904523536
-#define CARBON_E_0_5 1.64872127070012814684
-#define CARBON_E_0_25 1.28402541668774148407
-#define CARBON_E_0_125 1.13314845306682631682
-#define CARBON_PI 3.14159265358979323846
-#define CARBON_PI_2 (0.5 * CARBON_PI)
-#define CARBON_2PI (2 * CARBON_PI)
-#define CARBON_PI_4 (0.25 * CARBON_PI)
-#define CARBON_4PI (4 * CARBON_PI)
-#define CARBON_1_PI (1 / CARBON_PI)
-#define CARBON_1_2PI (1 / CARBON_2PI)
-#define CARBON_SQRT2 1.41421356237309504880
-#define CARBON_1_SQRT2 (1 / CARBON_SQRT2)
-#define CARBON_SQRT3 1.73205080756887729352
-#define CARBON_1_SQRT3 (1 / CARBON_SQRT3)
-#define CARBON_LOG2_E (1.4426950408889634074)
-#define CARBON_LOG2_10 (3.32192809488736234789)
-#define CARBON_I32_MAX 0x7fffffff
+#define CARBON_E        2.71828182845904523536
+#define CARBON_E_0_5    1.64872127070012814684
+#define CARBON_E_0_25   1.28402541668774148407
+#define CARBON_E_0_125  1.13314845306682631682
+#define CARBON_PI       3.14159265358979323846
+#define CARBON_PI_2     (0.5 * CARBON_PI)
+#define CARBON_2PI      (2 * CARBON_PI)
+#define CARBON_PI_4     (0.25 * CARBON_PI)
+#define CARBON_4PI      (4 * CARBON_PI)
+#define CARBON_1_PI     (1 / CARBON_PI)
+#define CARBON_1_2PI    (1 / CARBON_2PI)
+#define CARBON_SQRT2    1.41421356237309504880
+#define CARBON_1_SQRT2  (1 / CARBON_SQRT2)
+#define CARBON_SQRT3    1.73205080756887729352
+#define CARBON_1_SQRT3  (1 / CARBON_SQRT3)
+#define CARBON_LOG2_E   1.4426950408889634074
+#define CARBON_LOG2_10  3.32192809488736234789
+#define CARBON_I32_MAX  0x7fffffff
 #define CARBON_RAND_MAX CARBON_I32_MAX
-#define CARBON_F32_EPS 1.1920928955078125e-07
+#define CARBON_F32_EPS  1.1920928955078125e-07
+
+/**
+ * @brief Returns the smaller of two numbers.
+ * @param x The first number.
+ * @param y The second number.
+ */
 #define CARBON_MIN(x, y) (x < y ? x : y)
+
+/**
+ * @brief Returns the smaller of three numbers.
+ * @param x The first number.
+ * @param y The second number.
+ * @param z The third number.
+ */
 #define CARBON_MIN3(x, y, z) CARBON_MIN(x, CARBON_MIN(y, z))
+
+/**
+ * @brief Returns the larger of two numbers.
+ * @param x The first number.
+ * @param y The second number.
+ */
 #define CARBON_MAX(x, y) (x > y ? x : y)
+
+/**
+ * @brief Returns the larger of three numbers.
+ * @param x The first number.
+ * @param y The second number.
+ * @param z The third number.
+ */
 #define CARBON_MAX3(x, y, z) CARBON_MAX(x, CARBON_MAX(y, z))
+
+/**
+ * @brief Restricts a number between a minimum and a maximum value.
+ * @param x The number.
+ * @param min The minimum value.
+ * @param max The maximum value.
+ */
 #define CARBON_CLAMP(x, min, max) ((x <= min) ? min : (x >= max) ? max : x)
+
+/**
+ * @brief Returns an integer {-1, 0, 1} that indicates the sign of a number.
+ * @param x The number.
+ */
 #define CARBON_SIGN(x) (!x ? 0 : x < 0 ? -1 : 1)
+
 #define CARBON_STEP(edge, x) (x < edge ? 0 : 1)
 #define CARBON_SWAP(T, x, y) do { T z = x; x = y; y = z; } while (0)
 #define CARBON_LERP(a, b, t) ((a) + ((b) - (a)) * (t))
+
 // TODO: somehow perform bounds checking `CARBON_ASSERT(0 <= (i) && (i) < (m).rows && 0 <= (j) && (j) < (m).cols && "Matrix index out of bounds")`
 #define CARBON_MAT_AT(m, i, j) (m).items[(i) * (m).cols + (j)]
+
 // TODO: somehow perform bounds checking `CARBON_ASSERT(0 <= (i) && (i) < (r).cols && "Row index out of bounds")`
 #define CARBON_ROW_AT(r, i) (r).items[(i)]
+
 #define CARBON_MAT_PRINT(m) carbon_math_mat_print(m, #m)
 #define CARBON_ROW_PRINT(r) carbon_math_row_print(r, #r)
 
