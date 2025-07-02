f26e9c4acf73dbe081df737a63a8ea9da7f89b6a
iWas-Coder <wasymatieh01@gmail.com>
2025-07-02 20:46:31 +0200

    Simplified template specializations with if constexpr

--- src/carbon_log.h
+++ src/carbon_log.h
@@ -24,39 +24,42 @@
 #ifndef __cplusplus
 #define carbon_log__var_to_spec(x)              \
   _Generic((x),                                 \
-           usz: "%zu",                          \
-           isz: "%zd",                          \
-           u8: "%hhu",                          \
-           i8: "%hhd",                          \
-           u16: "%hu",                          \
-           i16: "%hd",                          \
-           u32: "%u",                           \
-           i32: "%d",                           \
-           u64: "%llu",                         \
-           i64: "%lld",                         \
-           f32: "%f",                           \
-           f64: "%lf",                          \
-           char: "%c",                          \
-           char *: "%s",                        \
+           usz:          "%zu",                 \
+           isz:          "%zd",                 \
+           u8:           "%hhu",                \
+           i8:           "%hhd",                \
+           u16:          "%hu",                 \
+           i16:          "%hd",                 \
+           u32:          "%u",                  \
+           i32:          "%d",                  \
+           u64:          "%llu",                \
+           i64:          "%lld",                \
+           f32:          "%f",                  \
+           f64:          "%lf",                 \
+           char:         "%c",                  \
+           char *:       "%s",                  \
            const char *: "%s",                  \
-           default: "<'Stuff' At %p>")
+           default:      "<'Stuff' At %p>")
 #else
-template <typename T> constexpr const char *carbon_log__var_to_spec(T)  { return "<'Stuff' At %p>"; }
-template <> constexpr const char *carbon_log__var_to_spec(usz)          { return "%zu"; }
-template <> constexpr const char *carbon_log__var_to_spec(isz)          { return "%zd"; }
-template <> constexpr const char *carbon_log__var_to_spec(u8)           { return "%hhu"; }
-template <> constexpr const char *carbon_log__var_to_spec(i8)           { return "%hhd"; }
-template <> constexpr const char *carbon_log__var_to_spec(u16)          { return "%hu"; }
-template <> constexpr const char *carbon_log__var_to_spec(i16)          { return "%hd"; }
-template <> constexpr const char *carbon_log__var_to_spec(u32)          { return "%u"; }
-template <> constexpr const char *carbon_log__var_to_spec(i32)          { return "%d"; }
-template <> constexpr const char *carbon_log__var_to_spec(u64)          { return "%llu"; }
-template <> constexpr const char *carbon_log__var_to_spec(i64)          { return "%lld"; }
-template <> constexpr const char *carbon_log__var_to_spec(f32)          { return "%f"; }
-template <> constexpr const char *carbon_log__var_to_spec(f64)          { return "%lf"; }
-template <> constexpr const char *carbon_log__var_to_spec(char)         { return "%c"; }
-template <> constexpr const char *carbon_log__var_to_spec(char *)       { return "%s"; }
-template <> constexpr const char *carbon_log__var_to_spec(const char *) { return "%s"; }
+template <typename T>
+constexpr auto carbon_log__var_to_spec(T) {
+  if constexpr      (CARBON_TYPE_IS_SAME(T, usz))          return "%zu";
+  else if constexpr (CARBON_TYPE_IS_SAME(T, isz))          return "%zd";
+  else if constexpr (CARBON_TYPE_IS_SAME(T, u8))           return "%hhu";
+  else if constexpr (CARBON_TYPE_IS_SAME(T, i8))           return "%hhd";
+  else if constexpr (CARBON_TYPE_IS_SAME(T, u16))          return "%hu";
+  else if constexpr (CARBON_TYPE_IS_SAME(T, i16))          return "%hd";
+  else if constexpr (CARBON_TYPE_IS_SAME(T, u32))          return "%u";
+  else if constexpr (CARBON_TYPE_IS_SAME(T, i32))          return "%d";
+  else if constexpr (CARBON_TYPE_IS_SAME(T, u64))          return "%llu";
+  else if constexpr (CARBON_TYPE_IS_SAME(T, i64))          return "%lld";
+  else if constexpr (CARBON_TYPE_IS_SAME(T, f32))          return "%f";
+  else if constexpr (CARBON_TYPE_IS_SAME(T, f64))          return "%lf";
+  else if constexpr (CARBON_TYPE_IS_SAME(T, char))         return "%c";
+  else if constexpr (CARBON_TYPE_IS_SAME(T, char *))       return "%s";
+  else if constexpr (CARBON_TYPE_IS_SAME(T, const char *)) return "%s";
+  else return "<'Stuff' At %p>";
+}
 #endif
 
 #define $(x) (CBN_Log_BoxedVar){carbon_log__var_to_spec(x), (void *) &x}
