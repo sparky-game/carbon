6052c3792008090ad79bcf10b2bb1904be4d3bef
iWas-Coder <wasymatieh01@gmail.com>
2025-10-09 23:03:29 +0200

    Improved camera translation and rotation logic
    More efficient. Also, it introduces the pitch clamp, so now it can't
    go upside down (yay!).

--- src/carbon_camera.c
+++ src/carbon_camera.c
@@ -5,6 +5,8 @@
 
 typedef struct CBN_Camera {
   CBN_Vec3 position;
+  f32 yaw;
+  f32 pitch;
   CBN_Quat rotation;
   CBN_Mat4 view;
   f32 fov;
@@ -34,6 +36,8 @@ void carbon_camera_reset(CBN_Camera *c, const CBN_DrawCanvas dc) {
     return;
   }
   c->position = CARBON_VEC3_ZERO;
+  c->yaw = 0;
+  c->pitch = 0;
   c->rotation = CARBON_QUAT_ID;
   c->view = carbon_math_mat4_view(c->position, c->rotation);
   c->fov = 60;
@@ -64,49 +68,56 @@ CBN_Mat4 carbon_camera_get_proj(const CBN_Camera *c) {
 }
 
 CARBON_INLINE void carbon_camera__update_view(CBN_Camera *c) {
-  if (!c) return;
   c->view = carbon_math_mat4_view(c->position, c->rotation);
 }
 
-CARBON_INLINE void carbon_camera__translate(CBN_Camera *c, CBN_Vec3 axis, f32 amount) {
+CARBON_INLINE void carbon_camera__translate(CBN_Camera *c, CBN_Vec3 v, f32 amount) {
   if (!c || !amount) return;
-  CBN_Vec3 r = carbon_math_vec3_rotate(axis, c->rotation);
-  r.y = 0;
-  if (carbon_math_vec3_len_squared(r) <= CARBON_EPS) r = CARBON_VEC3_FORWARD;
-  else r = carbon_math_vec3_norm(r);
-  CBN_Vec3 v = carbon_math_vec3_scale(r, amount);
+  v = carbon_math_vec3_scale(carbon_math_vec3_norm(v), amount);
   c->position = carbon_math_vec3_add(c->position, v);
   carbon_camera__update_view(c);
 }
 
+CARBON_INLINE void carbon_camera__translate_xz(CBN_Camera *c, CBN_Vec3 v, f32 amount) {
+  if (!c || !amount) return;
+  v = carbon_math_vec3_rotate(v, c->rotation);
+  v.y = 0;
+  carbon_camera__translate(c, v, amount);
+}
+
 void carbon_camera_move_forward(CBN_Camera *c, f32 amount) {
-  carbon_camera__translate(c, CARBON_VEC3_FORWARD, amount);
+  carbon_camera__translate_xz(c, CARBON_VEC3_FORWARD, amount);
 }
 
 void carbon_camera_move_backward(CBN_Camera *c, f32 amount) {
-  carbon_camera__translate(c, CARBON_VEC3_BACK, amount);
+  carbon_camera__translate_xz(c, CARBON_VEC3_BACK, amount);
 }
 
 void carbon_camera_move_left(CBN_Camera *c, f32 amount) {
-  carbon_camera__translate(c, CARBON_VEC3_LEFT, amount);
+  carbon_camera__translate_xz(c, CARBON_VEC3_LEFT, amount);
 }
 
 void carbon_camera_move_right(CBN_Camera *c, f32 amount) {
-  carbon_camera__translate(c, CARBON_VEC3_RIGHT, amount);
+  carbon_camera__translate_xz(c, CARBON_VEC3_RIGHT, amount);
 }
 
-CARBON_INLINE void carbon_camera__rotate(CBN_Camera *c, CBN_Vec3 axis, f32 amount) {
-  if (!c || !amount) return;
-  CBN_Quat r = carbon_math_quat_from_axis_angle(axis, amount);
-  c->rotation = carbon_math_quat_mult(r, c->rotation);
+CARBON_INLINE void carbon_camera__update_rotation(CBN_Camera *c) {
+  const CBN_Quat q_yaw = carbon_math_quat_from_axis_angle(CARBON_VEC3_UP, c->yaw);
+  const CBN_Vec3 v_pitch = carbon_math_vec3_rotate(CARBON_VEC3_RIGHT, q_yaw);
+  const CBN_Quat q_pitch = carbon_math_quat_from_axis_angle(v_pitch, c->pitch);
+  c->rotation = carbon_math_quat_mult(q_pitch, q_yaw);
   carbon_camera__update_view(c);
 }
 
 void carbon_camera_yaw(CBN_Camera *c, f32 amount) {
-  carbon_camera__rotate(c, CARBON_VEC3_UP, amount);
+  if (!c || !amount) return;
+  c->yaw += amount;
+  carbon_camera__update_rotation(c);
 }
 
 void carbon_camera_pitch(CBN_Camera *c, f32 amount) {
-  CBN_Vec3 v = carbon_math_vec3_rotate(CARBON_VEC3_RIGHT, c->rotation);
-  carbon_camera__rotate(c, v, amount);
+  static const f32 max_pitch = 89;
+  if (!c || !amount) return;
+  c->pitch = CARBON_CLAMP(c->pitch + amount, -max_pitch, max_pitch);
+  carbon_camera__update_rotation(c);
 }
