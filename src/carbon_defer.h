7a2e7a1f2113fdcaff88db50bf735b7e32a56421
iWas-Coder <wasymatieh01@gmail.com>
2025-10-25 22:40:52 +0200

    Improved a lot the C++ defer keyword implementation
    `defer({ free(ptr); });` -> `defer { free(ptr); };`... POGGERS!

--- src/carbon_defer.h
+++ src/carbon_defer.h
@@ -10,32 +10,38 @@
 #pragma once
 
 #ifdef __cplusplus
+
+/**
+ * @brief ...
+ */
+#define defer [[maybe_unused]] const auto carbon_defer__var = carbon_defer__adder + [&]()
+#define carbon_defer__var CARBON_PASTE(carbon_defer__, __LINE__)
 template <typename T>
 struct carbon_defer__raii {
   carbon_defer__raii(carbon_defer__raii &&) = delete;
   carbon_defer__raii(const carbon_defer__raii &) = delete;
   carbon_defer__raii &operator=(carbon_defer__raii &&) = delete;
   carbon_defer__raii &operator=(const carbon_defer__raii &) = delete;
-
-  ~carbon_defer__raii(void) { m_Cleanup(); }
-
+  ~carbon_defer__raii(void) { func(); }
   template <typename U>
-  carbon_defer__raii(U &&f) :  m_Cleanup{std::forward<U>(f)} {}
-
+  carbon_defer__raii(U &&f) : func{std::forward<U>(f)} {}
 private:
-  T m_Cleanup;
+  T func;
 };
-
 template <typename T>
 carbon_defer__raii<T> carbon_defer__impl(T &&f) { return {std::forward<T>(f)}; }
+struct carbon_defer__adder_t {
+  template <typename T>
+  carbon_defer__raii<std::decay_t<T>> operator+(T &&f) const noexcept {
+    return carbon_defer__impl(std::forward<T>(f));
+  }
+};
+[[maybe_unused]] inline constexpr carbon_defer__adder_t carbon_defer__adder {};
 
-/**
- * @brief ...
- */
-#define defer__var(x) CARBON_EXPAND_AND_PASTE(x, __LINE__)
-#define defer(L) [[maybe_unused]] const auto& defer__var(carbon_defer__obj) = carbon_defer__impl([&]() L)
 #else
+
 // TODO: implement the `defer` keyword in C
+
 #endif
 
 // Local Variables:
