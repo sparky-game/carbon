e303893c8d03b0e2a2b1a4898a99fd3189dd3519
iWas-Coder <wasymatieh01@gmail.com>
2025-12-23 23:24:06 +0100

    Implemented simple and generic Insertion Sort algorithm (#64)

---
+++ src/carbon_sort.c
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+// Copyright (C) Wasym A. Alonso. All Rights Reserved.
+
+#include "carbon.inc"
+
+void carbon_sort_insertion(void *p, usz n, usz w, CBN_Sort_CmpFunc f_cmp) {
+  u8 *a = (u8 *) p;
+  u8 t[w];
+  for (usz i = 1; i < n; ++i) {
+    carbon_memory_copy(t, a + w*i, w);
+    usz j;
+    for (j = i; j > 0 && f_cmp(a + w*(j-1), t) > 0; --j) {
+      carbon_memory_copy(a + w*j, a + w*(j-1), w);
+    }
+    carbon_memory_copy(a + w*j, t, w);
+  }
+}
---
+++ src/carbon_sort.h
@@ -0,0 +1,30 @@
+/*
+**  $$=====================$$
+**  ||       Sorting       ||
+**  $$=====================$$
+*/
+
+// SPDX-License-Identifier: AGPL-3.0-only
+// Copyright (C) Wasym A. Alonso. All Rights Reserved.
+
+#pragma once
+
+/**
+ * @brief ...
+ *
+ * ...
+ *
+ * @return ...
+ */
+typedef i32 (*CBN_Sort_CmpFunc)(const void *, const void *);
+CBNDEF_T(cbn::sort, CmpFunc, CBN_Sort_CmpFunc);
+
+/**
+ * @brief ...
+ * @param p ...
+ * @param n ...
+ * @param w ...
+ * @param f_cmp ...
+ */
+void carbon_sort_insertion(void *p, usz n, usz w, CBN_Sort_CmpFunc f_cmp);
+CBNDEF_FN(cbn::sort, Insertion, carbon_sort_insertion);
