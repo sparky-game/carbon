8074c702d87e70aa3dc2c1ce0b3fbec294741bda
iWas-Coder <wasymatieh01@gmail.com>
2026-02-16 04:47:19 +0100

    fix: function doesn't need to return nothing if takes ref as param

--- how_to/30_plasma.cc
+++ how_to/30_plasma.cc
@@ -18,11 +18,10 @@ namespace shader {
     for(;i.y++<8.;o+=(sin(v.xyyx)+1.)*abs(v.x-v.y))v+=cos(v.yx*i.y+i+t)/i.y+.7;
     o=tanh(5.*exp(l.x-4.-p.y*vec4(-1,1,2,0))/o);
   */
-  static vec4 Run(vec4 &o, vec4 FC, vec2 r, f64 t) {
+  static void Run(vec4 &o, vec4 FC, vec2 r, f64 t) {
     vec2 p=(FC.xy*2.-r)/r.y,l,i,v=p*(l+=4.-4.*abs(.7-dot(p,p)));
     for(;i.y++<8.;o+=(sin(v.xyyx())+1.)*abs(v.x-v.y))v+=cos(v.yx()*i.y+i+t)/i.y+.7;
     o=tanh(5.*exp(l.x-4.-p.y*vec4(-1,1,2,0))/o);
-    return o;
   }
 
   void Render(cbn::DrawCanvas &dc, const f64 dt) {
