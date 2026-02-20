c862bcaeb6e987d8e67732e1b8aac16fdccafa29
iWas-Coder <wasymatieh01@gmail.com>
2026-02-20 06:24:30 +0100

    fix: make build scripts carry on the exit code from the build itself

--- src_build/make.bat
+++ src_build/make.bat
@@ -6,4 +6,6 @@ set "OUT=%~dpn0.exe"
 set "CXX=c++ -std=c++20 -O3"
 
 %CXX% "%IN%" -o "%OUT%" && "%OUT%" %*
-del "%OUT%"
+set "EXIT_CODE=%ERRORLEVEL%"
+if exist "%OUT%" del "%OUT%"
+exit /b %EXIT_CODE%
--- src_build/make.sh
+++ src_build/make.sh
@@ -4,5 +4,5 @@ IN="${0%.*}.cc"
 OUT="${0%.*}"
 CXX="c++ -std=c++20 -O3"
 
+trap "rm -f $OUT" EXIT
 $CXX $IN -o $OUT && $OUT "$@"
-rm $OUT
