38aac5646845e6846bba51bafc6caf3e11615199
iWas-Coder <wasymatieh01@gmail.com>
2026-02-27 06:13:19 +0100

    Simplified builtin BS where it isn't needed

--- src/carbon_memory.c
+++ src/carbon_memory.c
@@ -20,34 +20,22 @@ void *carbon_memory_realloc(void *p, usz size) {
 }
 
 void *carbon_memory_copy(void * restrict dst, const void * restrict src, usz n) {
-#if CARBON_HAS_BUILTIN(__builtin_memcpy)
-  return __builtin_memcpy(dst, src, n);
-#else
   u8 *d = (u8 *) dst;
   const u8 *s = (const u8 *) src;
   while (n--) *d++ = *s++;
   return dst;
-#endif
 }
 
 i32 carbon_memory_cmp(const void *v1, const void *v2, usz n) {
-#if CARBON_HAS_BUILTIN(__builtin_memcmp)
-  return __builtin_memcmp(v1, v2, n);
-#else
   const u8 *l = (const u8 *) v1, *r = (const u8 *) v2;
   for (; n && *l == *r; --n, ++l, ++r);
   return n ? *l - *r : 0;
-#endif
 }
 
 void *carbon_memory_set(void *dst, i32 c, usz n) {
-#if CARBON_HAS_BUILTIN(__builtin_memset)
-  return __builtin_memset(dst, c, n);
-#else
   u8 *d = (u8 *) dst;
   while (n--) *d++ = (u8) c;
   return dst;
-#endif
 }
 
 void carbon_memory_free(void *p) {
