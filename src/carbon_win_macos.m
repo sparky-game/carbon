fee5f23d12a3917918a7643bd27218de7b887b89
iWas-Coder <wasymatieh01@gmail.com>
2026-03-15 03:15:26 +0100

    Added lookup table of KeyCode's for macOS

--- src/carbon_win_macos.m
+++ src/carbon_win_macos.m
@@ -4,7 +4,8 @@
 #pragma once
 
 #import <Metal/Metal.h>
-#import <AppKit/NSView.h>
+#import <Carbon/Carbon.h>
+#import <AppKit/AppKit.h>
 #import <QuartzCore/CAMetalLayer.h>
 
 @interface CBNView : NSView
@@ -104,4 +105,80 @@ CBNINL void carbon_win__renderer_shutdown(void) {
   [carbon_win__mtl_layer    release];
   [carbon_win__mtl_queue    release];
   [carbon_win__mtl_device   release];
+}
+
+CBNINL CBN_Win_KeyCode carbon_win__map_key_code(u16 key) {
+  // <https://forums.macrumors.com/threads/nsevent-keycode-list.780577/post-8428116>
+  // <https://boredzo.org/blog/wp-content/uploads/2007/05/IMTx-virtual-keycodes.pdf>
+  switch (key) {
+  case kVK_ANSI_A:       return CBN_Win_KeyCode_A;
+  case kVK_ANSI_B:       return CBN_Win_KeyCode_B;
+  case kVK_ANSI_C:       return CBN_Win_KeyCode_C;
+  case kVK_ANSI_D:       return CBN_Win_KeyCode_D;
+  case kVK_ANSI_E:       return CBN_Win_KeyCode_E;
+  case kVK_ANSI_F:       return CBN_Win_KeyCode_F;
+  case kVK_ANSI_G:       return CBN_Win_KeyCode_G;
+  case kVK_ANSI_H:       return CBN_Win_KeyCode_H;
+  case kVK_ANSI_I:       return CBN_Win_KeyCode_I;
+  case kVK_ANSI_J:       return CBN_Win_KeyCode_J;
+  case kVK_ANSI_K:       return CBN_Win_KeyCode_K;
+  case kVK_ANSI_L:       return CBN_Win_KeyCode_L;
+  case kVK_ANSI_M:       return CBN_Win_KeyCode_M;
+  case kVK_ANSI_N:       return CBN_Win_KeyCode_N;
+  case kVK_ANSI_O:       return CBN_Win_KeyCode_O;
+  case kVK_ANSI_P:       return CBN_Win_KeyCode_P;
+  case kVK_ANSI_Q:       return CBN_Win_KeyCode_Q;
+  case kVK_ANSI_R:       return CBN_Win_KeyCode_R;
+  case kVK_ANSI_S:       return CBN_Win_KeyCode_S;
+  case kVK_ANSI_T:       return CBN_Win_KeyCode_T;
+  case kVK_ANSI_U:       return CBN_Win_KeyCode_U;
+  case kVK_ANSI_V:       return CBN_Win_KeyCode_V;
+  case kVK_ANSI_W:       return CBN_Win_KeyCode_W;
+  case kVK_ANSI_X:       return CBN_Win_KeyCode_X;
+  case kVK_ANSI_Y:       return CBN_Win_KeyCode_Y;
+  case kVK_ANSI_Z:       return CBN_Win_KeyCode_Z;
+  case kVK_ANSI_0:       return CBN_Win_KeyCode_Zero;
+  case kVK_ANSI_1:       return CBN_Win_KeyCode_One;
+  case kVK_ANSI_2:       return CBN_Win_KeyCode_Two;
+  case kVK_ANSI_3:       return CBN_Win_KeyCode_Three;
+  case kVK_ANSI_4:       return CBN_Win_KeyCode_Four;
+  case kVK_ANSI_5:       return CBN_Win_KeyCode_Five;
+  case kVK_ANSI_6:       return CBN_Win_KeyCode_Six;
+  case kVK_ANSI_7:       return CBN_Win_KeyCode_Seven;
+  case kVK_ANSI_8:       return CBN_Win_KeyCode_Eight;
+  case kVK_ANSI_9:       return CBN_Win_KeyCode_Nine;
+  case kVK_ANSI_Grave:   return CBN_Win_KeyCode_BackQuote;
+  case kVK_F1:           return CBN_Win_KeyCode_F1;
+  case kVK_F2:           return CBN_Win_KeyCode_F2;
+  case kVK_F3:           return CBN_Win_KeyCode_F3;
+  case kVK_F4:           return CBN_Win_KeyCode_F4;
+  case kVK_F5:           return CBN_Win_KeyCode_F5;
+  case kVK_F6:           return CBN_Win_KeyCode_F6;
+  case kVK_F7:           return CBN_Win_KeyCode_F7;
+  case kVK_F8:           return CBN_Win_KeyCode_F8;
+  case kVK_F9:           return CBN_Win_KeyCode_F9;
+  case kVK_F10:          return CBN_Win_KeyCode_F10;
+  case kVK_F11:          return CBN_Win_KeyCode_F11;
+  case kVK_F12:          return CBN_Win_KeyCode_F12;
+  case kVK_Escape:       return CBN_Win_KeyCode_Escape;
+  case kVK_Tab:          return CBN_Win_KeyCode_Tab;
+  case kVK_CapsLock:     return CBN_Win_KeyCode_CapsLock;
+  case kVK_Shift:        return CBN_Win_KeyCode_LeftShift;
+  case kVK_Control:      return CBN_Win_KeyCode_LeftCtrl;
+  case kVK_Command:      return CBN_Win_KeyCode_LeftMeta;
+  case kVK_Option:       return CBN_Win_KeyCode_LeftAlt;
+  case kVK_Space:        return CBN_Win_KeyCode_Space;
+  case kVK_RightOption:  return CBN_Win_KeyCode_RightAlt;
+  case kVK_RightControl: return CBN_Win_KeyCode_RightCtrl;
+  case kVK_RightShift:   return CBN_Win_KeyCode_RightShift;
+  case kVK_Return:       return CBN_Win_KeyCode_Return;
+  case kVK_UpArrow:      return CBN_Win_KeyCode_UpArrow;
+  case kVK_DownArrow:    return CBN_Win_KeyCode_DownArrow;
+  case kVK_LeftArrow:    return CBN_Win_KeyCode_LeftArrow;
+  case kVK_RightArrow:   return CBN_Win_KeyCode_RightArrow;
+    // `CBN_Win_KeyCode_RightMeta` not supported
+  default:
+    CARBON_UNREACHABLE;
+    return CBN_Win_KeyCode_Count;
+  }
 }
