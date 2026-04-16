c9c852cac999152b12c5d32a13fc49189a4337d4
iWas-Coder <wasymatieh01@gmail.com>
2026-04-16 06:50:38 +0200

    Implemented comparison operators (eq,neq) for SlotMap::Key type

--- src/carbon_slotmap.hh
+++ src/carbon_slotmap.hh
@@ -3,9 +3,11 @@
 #ifdef __cplusplus
 
 struct CBN_SlotMap_Key : CBN_SlotMap_Key_t {
-  const char *ToString(void) const {
-    return carbon_slotmap_key_to_cstr(*this);
-  }
+  bool operator==(const CBN_SlotMap_Key &k) const { return id == k.id && gen == k.gen; }
+
+  bool operator!=(const CBN_SlotMap_Key &k) const { return !(*this == k); }
+
+  const char *ToString(void) const { return carbon_slotmap_key_to_cstr(*this); }
 };
 
 template <typename T>
@@ -22,9 +24,7 @@ struct CBN_SlotMap_tt : CBN_SlotMap_t {
   CBN_SlotMap_tt(const CBN_SlotMap_tt &) = delete;
   CBN_SlotMap_tt &operator=(const CBN_SlotMap_tt &) = delete;
 
-  CBN_SlotMap_tt(CBN_SlotMap_tt &&sm) : CBN_SlotMap_t{sm} {
-    carbon_memory_set(&sm, 0, sizeof(sm));
-  }
+  CBN_SlotMap_tt(CBN_SlotMap_tt &&sm) : CBN_SlotMap_t{sm} { carbon_memory_set(&sm, 0, sizeof(sm)); }
 
   CBN_SlotMap_tt &operator=(CBN_SlotMap_tt &&sm) {
     if (this != &sm) {
@@ -35,35 +35,23 @@ struct CBN_SlotMap_tt : CBN_SlotMap_t {
     return *this;
   }
 
-  ~CBN_SlotMap_tt(void) {
-    Free();
-  }
+  ~CBN_SlotMap_tt(void) { Free(); }
 
-  void Free(void) {
-    carbon_slotmap_destroy((CBN_SlotMap *) this);
-  }
+  void Free(void) { carbon_slotmap_destroy((CBN_SlotMap *) this); }
 
-  Key Push(const value_type &value) {
-    return carbon_slotmap_push((CBN_SlotMap *) this, (void *) &value);
-  }
+  Key Push(const value_type &value) { return carbon_slotmap_push((CBN_SlotMap *) this, (void *) &value); }
 
-  u8 Remove(const Key k) {
-    return carbon_slotmap_remove((CBN_SlotMap *) this, k);
-  }
+  u8 Remove(const Key k) { return carbon_slotmap_remove((CBN_SlotMap *) this, k); }
 
-  iterator begin(void) const {
-    return ((data_type *) &data)->begin();
-  }
+  iterator begin(void) const { return ((data_type *) &data)->begin(); }
 
-  iterator end(void) const {
-    return ((data_type *) &data)->end();
-  }
+  iterator end(void) const { return ((data_type *) &data)->end(); }
 
   cbn::Opt<value_type> operator[](const Key k) {
     value_type x;
     if (!carbon_slotmap_lookup((CBN_SlotMap *) this, k, &x)) return {};
     return x;
-  }  
+  }
 };
 
 #endif
