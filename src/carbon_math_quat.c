07a229eaee6e135a66b9a6db9f703e4adf6a17f5
iWas-Coder <wasymatieh01@gmail.com>
2026-05-19 22:09:35 +0200

    fix: moved some funcs around

--- src/carbon_math_quat.c
+++ src/carbon_math_quat.c
@@ -5,17 +5,6 @@ CBN_Quat carbon_math_quat_id(void) {
   return carbon_math_quat(0, 0, 0, 1);
 }
 
-CBN_Quat carbon_math_quat_conj(CBN_Quat q) {
-  return carbon_math_quat(-q.x, -q.y, -q.z, q.w);
-}
-
-CBN_Quat carbon_math_quat_mult(CBN_Quat p, CBN_Quat q) {
-  return carbon_math_quat(p.x * q.w + p.y * q.z - p.z * q.y + p.w * q.x,
-                          -p.x * q.z + p.y * q.w + p.z * q.x + p.w * q.y,
-                          p.x * q.y - p.y * q.x + p.z * q.w + p.w * q.z,
-                          -p.x * q.x - p.y * q.y - p.z * q.z + p.w * q.w);
-}
-
 CBN_Quat carbon_math_quat_from_euler(CBN_Vec3 v) {
   f32 hx = CARBON_TO_RADIANS(v.x)/2, hy = CARBON_TO_RADIANS(v.y)/2, hz = CARBON_TO_RADIANS(v.z)/2;
   f32 sx = carbon_math_sin(hx), sy = carbon_math_sin(hy), sz = carbon_math_sin(hz);
@@ -28,6 +17,17 @@ CBN_Quat carbon_math_quat_from_axis_angle(CBN_Vec3 axis, f32 angle) {
   const f32 ha = CARBON_TO_RADIANS(angle)/2;
   f32 s = carbon_math_sin(ha), c = carbon_math_cos(ha);
   return carbon_math_quat(s * axis.x, s * axis.y, s * axis.z, c);
+}
+
+CBN_Quat carbon_math_quat_conj(CBN_Quat q) {
+  return carbon_math_quat(-q.x, -q.y, -q.z, q.w);
+}
+
+CBN_Quat carbon_math_quat_mult(CBN_Quat p, CBN_Quat q) {
+  return carbon_math_quat(p.x * q.w + p.y * q.z - p.z * q.y + p.w * q.x,
+                          -p.x * q.z + p.y * q.w + p.z * q.x + p.w * q.y,
+                          p.x * q.y - p.y * q.x + p.z * q.w + p.w * q.z,
+                          -p.x * q.x - p.y * q.y - p.z * q.z + p.w * q.w);
 }
 
 f32 carbon_math_quat_len_squared(CBN_Quat q) {
