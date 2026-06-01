de3a185cefb551d9e062db965bd885b3c8c9396a
iWas-Coder <wasymatieh01@gmail.com>
2026-06-01 23:28:11 +0200

    [WIP] Added 16-byte alignment to memory allocator

--- src/carbon_memory.c
+++ src/carbon_memory.c
@@ -1,12 +1,23 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 // Copyright (C) Wasym A. Alonso. All Rights Reserved.
 
-#define CARBON_MEMORY__HDR_SZ (sizeof(usz))
+#define CARBON_MEMORY__MAX_ALIGN 16
+#define CARBON_MEMORY__HDR_SZ ((sizeof(usz) + CARBON_MEMORY__MAX_ALIGN - 1) & ~(CARBON_MEMORY__MAX_ALIGN - 1))
+
+#ifdef _WIN32
+#define CARBON_MEMORY__ALLOC(sz) _aligned_malloc((sz), CARBON_MEMORY__MAX_ALIGN)
+#define CARBON_MEMORY__REALLOC(p, sz) _aligned_realloc((p), (sz), CARBON_MEMORY__MAX_ALIGN)
+#define CARBON_MEMORY__FREE(p) _aligned_free((p))
+#else
+#define CARBON_MEMORY__ALLOC(sz) __builtin_malloc((sz))
+#define CARBON_MEMORY__REALLOC(p, sz) __builtin_realloc((p), (sz))
+#define CARBON_MEMORY__FREE(p) __builtin_free((p))
+#endif
 
 static usz carbon_memory__usage;
 
 void *carbon_memory_alloc(usz size) {
-  u8 *p = __builtin_malloc(size + CARBON_MEMORY__HDR_SZ);
+  u8 *p = CARBON_MEMORY__ALLOC(size + CARBON_MEMORY__HDR_SZ);
   CBN_ASSERT(p && "failed to allocate memory");
   *(usz *)p = size;
   carbon_memory__usage += size;
@@ -23,7 +34,7 @@ void *carbon_memory_realloc(void *p, usz size) {
   if (!p) return carbon_memory_alloc(size);
   u8 *raw_p = (u8 *)p - CARBON_MEMORY__HDR_SZ;
   usz old_sz = *(usz *)raw_p;
-  u8 *new_p = __builtin_realloc(raw_p, size + CARBON_MEMORY__HDR_SZ);
+  u8 *new_p = CARBON_MEMORY__REALLOC(raw_p, size + CARBON_MEMORY__HDR_SZ);
   CBN_ASSERT(new_p && "failed to reallocate memory");
   *(usz *)new_p = size;
   carbon_memory__usage -= old_sz;
@@ -35,7 +46,7 @@ void carbon_memory_free(void *p) {
   if (!p) return;
   u8 *raw_p = (u8 *)p - CARBON_MEMORY__HDR_SZ;
   carbon_memory__usage -= *(usz *)raw_p;
-  __builtin_free(raw_p);
+  CARBON_MEMORY__FREE(raw_p);
 }
 
 usz carbon_memory_usage(void) {
@@ -43,8 +54,8 @@ usz carbon_memory_usage(void) {
 }
 
 void *carbon_memory_copy(void *dst, const void *src, usz n) {
-  u8 *d = (u8 *) dst;
-  const u8 *s = (const u8 *) src;
+  u8 *d = (u8 *)dst;
+  const u8 *s = (const u8 *)src;
   if (d == s || !n) return dst;
   if (d < s) while (n--) *d++ = *s++;
   else while (n--) d[n] = s[n];
@@ -52,20 +63,20 @@ void *carbon_memory_copy(void *dst, const void *src, usz n) {
 }
 
 void *carbon_memory_copy_raw(void *dst, const void *src, usz n) {
-  u8 *d = (u8 *) dst;
-  const u8 *s = (const u8 *) src;
+  u8 *d = (u8 *)dst;
+  const u8 *s = (const u8 *)src;
   while (n--) *d++ = *s++;
   return dst;
 }
 
 i32 carbon_memory_cmp(const void *v1, const void *v2, usz n) {
-  const u8 *l = (const u8 *) v1, *r = (const u8 *) v2;
+  const u8 *l = (const u8 *)v1, *r = (const u8 *)v2;
   for (; n && *l == *r; --n, ++l, ++r);
   return n ? *l - *r : 0;
 }
 
 void *carbon_memory_set(void *dst, i32 c, usz n) {
-  u8 *d = (u8 *) dst;
-  while (n--) *d++ = (u8) c;
+  u8 *d = (u8 *)dst;
+  while (n--) *d++ = (u8)c;
   return dst;
 }
