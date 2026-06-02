bc5264e74b9bea91f9f0851a5cd8a5c8bd66583a
iWas-Coder <wasymatieh01@gmail.com>
2026-06-02 03:12:04 +0200

    Customized `stb_truetype.h` library with our own types and functions

--- src/carbon_font.c
+++ src/carbon_font.c
@@ -1,6 +1,26 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 // Copyright (C) Wasym A. Alonso. All Rights Reserved.
 
+#define stbtt_uint8       u8
+#define stbtt_int8        i8
+#define stbtt_uint16      u16
+#define stbtt_int16       i16
+#define stbtt_uint32      u32
+#define stbtt_int32       i32
+#define STBTT_ifloor(x)   ((int) carbon_math_floor(x))
+#define STBTT_iceil(x)    ((int) carbon_math_ceil(x))
+#define STBTT_sqrt(x)     carbon_math_sqrt(x)
+#define STBTT_pow(x,y)    carbon_math_pow(x,y)
+#define STBTT_fmod(x,y)   carbon_math_fmod(x,y)
+#define STBTT_cos(x)      carbon_math_cos(x)
+#define STBTT_acos(x)     carbon_math_acos(x)
+#define STBTT_fabs(x)     carbon_math_abs(x)
+#define STBTT_malloc(x,u) ((void)(u),carbon_memory_alloc(x))
+#define STBTT_free(x,u)   ((void)(u),carbon_memory_free(x))
+#define STBTT_assert(x)   CBN_ASSERT(x)
+#define STBTT_strlen(x)   carbon_string_len(x)
+#define STBTT_memcpy      carbon_memory_copy
+#define STBTT_memset      carbon_memory_set
 #define STB_TRUETYPE_IMPLEMENTATION
 #include "../thirdparty/stb/stb_truetype.h"
 
