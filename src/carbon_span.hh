7794ce845573be4d682f12874aded2a51719db1d
iWas-Coder <wasymatieh01@gmail.com>
2026-07-16 02:38:08 +0200

    Added C-array constructor option for Span type

--- src/carbon_span.hh
+++ src/carbon_span.hh
@@ -9,6 +9,8 @@ struct CBN_Span_tt : CBN_Span_t {
 
   CBN_Span_tt(void) : CBN_Span_t{} {}
   CBN_Span_tt(auto *p, usz sz) : CBN_Span_t{carbon_span_from_buf((void *)p, sz)} {}
+  template <usz N>
+  CBN_Span_tt(value_type (&xs)[N]) : CBN_Span_tt{xs, N} {}
   CBN_Span_tt(const CBN_List_tt<value_type> &l) : CBN_Span_t{carbon_span_from_list((CBN_List *)&l)} {}
 
   CBN_Span_tt SubSpan(usz offset, usz count) const { return carbon_span_subspan(*this, offset, count); }
@@ -17,7 +19,13 @@ struct CBN_Span_tt : CBN_Span_t {
 
   iterator end(void) const { return (iterator)data + size; }
 
-  value_type &operator[](usz idx) { return carbon_span_at(value_type, *this, idx); }
+  value_type &operator[](usz idx) const { return carbon_span_at(value_type, *this, idx); }
 };
+
+template <typename T, usz N>
+CBN_Span_tt(T (&)[N]) -> CBN_Span_tt<T>;
+
+template <typename T>
+CBN_Span_tt(const CBN_List_tt<T> &) -> CBN_Span_tt<T>;
 
 #endif
