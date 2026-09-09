9db6d2f37626bb1a3baafe9efee4bb4f85ca4688
iWas-Coder <wasymatieh01@gmail.com>
2026-09-09 23:27:37 +0200

    fix: sprite drawing clipping corrected with ceilf

--- src/carbon_drawcanvas.c
+++ src/carbon_drawcanvas.c
@@ -235,8 +235,14 @@ void carbon_drawcanvas_sprite(CBN_DrawCanvas *dc, const CBN_Sprite *s, CBN_Vec2 
   const CBN_Rect r_sp = carbon_math_rect_v(position, sw, sh);
   CBN_Rect xywh = carbon_math_rect_intersection(r_dc, r_sp);
   if (xywh.w <= 0 || xywh.h <= 0) return;
-  xywh.x = carbon_math_floor(xywh.x), xywh.y = carbon_math_floor(xywh.y);
-  xywh.w = carbon_math_min(xywh.w, r_dc.w - xywh.x), xywh.h = carbon_math_min(xywh.h, r_dc.h - xywh.y);
+  {// Clipping
+    f32 ox = xywh.x, oy = xywh.y;
+    xywh.x = carbon_math_floor(ox), xywh.y = carbon_math_floor(oy);
+    xywh.w = carbon_math_ceil(ox + xywh.w) - xywh.x;
+    xywh.w = carbon_math_min(xywh.w, r_dc.w - xywh.x);
+    xywh.h = carbon_math_ceil(oy + xywh.h) - xywh.y;
+    xywh.h = carbon_math_min(xywh.h, r_dc.h - xywh.y);
+  }
   u32 *p_dc = dc->pixels + (usz)(xywh.y * r_dc.w + xywh.x);
   if (scale.x == 1 && scale.y == 1 && position.x == carbon_math_floor(position.x) && position.y == carbon_math_floor(position.y)) {
     const usz start_x = carbon_math_max(0, xywh.x - r_sp.x);
